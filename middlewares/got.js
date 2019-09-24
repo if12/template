3fa81f92fd69f53621523cfd46c5ed506f4cbd4b
张猴 <kaihao.zkh@alibaba-inc.com>
2019-09-25 00:40:17 +0800

    support http download

--- middlewares/got.js
+++ middlewares/got.js
@@ -5,13 +5,28 @@
 const { execSync } = require('child_process');
 const { copy, remove } = require('fs-extra');
 const ora = require('ora');
+const download = require('download-git-repo');
 const { resolveCWD } = require('../utils/');
 
 // TODO 可以支持多种拉取模版的能力
 module.exports = function got(ctx) {
-  const { template, contentDir = '', registry = '', config = {} } = ctx;
+  const { downloadType = 'http', template, contentDir = '', registry = '', config = {} } = ctx;
   const templateName = template || registry;
   const { BRANCH, DOMAIN, GROUP, CLONE_DIR } = config;
+
+  const downloadActions = {
+    clone: () => {
+      execSync(`git clone -b ${BRANCH} ${registry} ${CLONE_DIR} --depth=1`, {
+        stdio: 'pipe',
+      });
+    },
+
+    http: () => {
+      download(`${DOMAIN}:${GROUP}/${templateName}#${BRANCH}`, CLONE_DIR, {}, err => {
+        throw err;
+      });
+    },
+  };
 
   // 读取本地
   if (contentDir) {
@@ -21,9 +36,7 @@ module.exports = function got(ctx) {
     const spinner = ora(`下载${templateName}模版...`).start();
 
     try {
-      execSync(`git clone -b ${BRANCH} ${registry} ${CLONE_DIR} --depth=1`, {
-        stdio: 'pipe',
-      });
+      downloadActions[downloadType]();
     } catch (err) {
       spinner.fail();
       throw new Error(`获取 ${registry} 失败! \n原因是 ${err.message}`);
