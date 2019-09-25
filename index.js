2d4609a06e527a100ce3c2739902abc243caa841
monkindey <monkindey@163.com>
2019-09-25 22:31:53 +0800

    every templateRender will new WareHub

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const middlewares = require('./middlewares/index');
 const { runCmds, resolveCWD } = require('./utils/index');
 const WareHub = require('./warehub');
 
-const pickerWareHub = new WareHub();
-
 process.on('unhandledRejection', err => {
   console.error(chalk.red(err.stack));
 });
 
 module.exports = function templateRender(context = {}) {
+  const pickerWareHub = new WareHub();
   context.config = { ...config, ...context.config };
 
   return (
