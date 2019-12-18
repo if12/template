1dda2f26880a9b235356c834f806496349be24d2
monkindey <monkindey@163.com>
2019-12-18 22:46:42 +0800

    加了本地 copy test

--- test.js
+++ test.js
@@ -1,26 +1,37 @@
 const path = require('path');
 const fse = require('fs-extra');
+const meow = require('meow');
 const templateRender = require('./index');
 
 const fixtures = path.resolve(process.cwd(), 'fixtures');
+const cli = meow('', {
+  contentDir: {
+    type: 'string',
+  },
+});
+
+const { contentDir } = cli.flags;
+const templateConf = contentDir ? { templateDidMount: () => [] } : {};
+const options = {
+  template: 'node-template',
+  dest: fixtures,
+  contentDir,
+  templateConf,
+  config: {
+    GROUP: 'if12',
+    TEMPLATE_CONTENT_DIR: '',
+  },
+  variable: {
+    group: 'if12',
+    project: 'test-template',
+    version: '0.0.1',
+    description: '',
+  },
+};
 
 fse
   .ensureDir('./fixtures')
-  .then(() =>
-    templateRender({
-      template: 'node-template',
-      dest: fixtures,
-      config: {
-        GROUP: 'if12',
-      },
-      variable: {
-        group: 'if12',
-        project: 'test-template',
-        version: '0.0.1',
-        description: '',
-      },
-    })
-  )
+  .then(() => templateRender(options))
   .then(() => {
     console.log('初始化模版成功');
   });
